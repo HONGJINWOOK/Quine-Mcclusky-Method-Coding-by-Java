7ad1f6b9aa553c98685adc9c3665722b6cde132d
HONGJINWOOK <37397547+HONGJINWOOK@users.noreply.github.com>
2020-04-23 11:21:55 +0900

    Update and rename tabular to tabular.py

--- tabular.py
+++ tabular.py
@@ -79,24 +79,24 @@ class Tabular: #Tabular 확인
         #값을 반복해서 확인
         for value in self._values:
 
-            # Count number of 1's in value's bit equivalent
-            count = self.__get_bits(value).count("1")
+            #1의 개수에 해당하는 비트 확인
+            cnt = self.__get_bits(value).count("1")
 
-            # Add count to proper group
-            groups[count].append(Minterm([value], self.__get_bits(value)))
+            #맞는 그룹에 카운트 추가
+            groups[cnt].append(Minterm([value], self.__get_bits(value)))
         
         return groups
         
     def __get_prime_implicants(self, groups = None): #prime implicants 정의
 
         if groups == None:
-            groups = self.__initial_group()
+            groups = self.__grouping()
         
-        # If there is only 1 group, return all the minterms in it
+        #그룹이 하나라면 모든 minterm return
         if len(groups) == 1:
             return groups[0]
         
-        # Try comparing the rest
+        #나머지 비교
         else:
             unused = []
             comparisons = range(len(groups) - 1)
@@ -106,31 +106,73 @@ class Tabular: #Tabular 확인
                 group1 = groups[compare]
                 group2 = groups[compare + 1]
 
-                # Compare every term in group1 with every term in group2
-                for term1 in group1:
-                    for term2 in group2:
+                # 그룹1의 모든 요소를 그룹 2의 모든 요소와 비교
+                for elem1 in group1:
+                    for elem2 in group2:
                         
-                        # Try combining it
-                        term3 = term1.combine(term2)
+                        #결합하기
+                        elem3 = elem1.combine(elem2)
 
-                        # Only add it to the new group if term3 is not None
-                        #   term3 will only be None if term1 and term2 could not
-                        #   be combined
-                        if term3 != None:
-                            term1.use()
-                            term2.use()
-                            if term3 not in new_groups[compare]:
-                                new_groups[compare].append(term3)
+                        # elem3이 None이 아닌 경우에 새 그룹에 추가
+                        # elem3은 elem1과 elem2가 결합이 실패하면 None
+                        if elem3 != None:
+                            elem1.use()
+                            elem2.use()
+                            if elem3 not in new_groups[compare]:
+                                new_groups[compare].append(elem3)
             
-            # Get list of all unused minterms
+            # 미사용된 minterm
             for group in groups:
-                for term in group:
-                    if not term.used() and term not in unused:
-                        unused.append(term)
+                for elem in group:
+                    if not elem.used() and elem not in unused:
+                        unused.append(elem)
             
             # Add recursive call
-            for term in self.__get_prime_implicants(new_groups):
-                if not term.used() and term not in unused:
-                    unused.append(term)
+            for elem in self.__get_prime_implicants(new_groups):
+                if not elem.used() and elem not in unused:
+                    unused.append(elem)
 
             return unused
+
+def __solution(self): #필요한 최소량의 prime implicant 확인
+        
+        #PI 확인
+        prime_implicants = self.__get_prime_implicants(self.__grouping())
+        
+        #하나의 Implicant만 확인
+        #EPI
+        essential_prime_implicants = []
+        values_used = [False] * len(self._values)
+
+        for i in range(len(self._values)):
+            value = self._values[i]
+
+            uses = 0
+            last = None
+            for minterm in prime_implicants:
+                if value in minterm.get_values():
+                    uses += 1
+                    last = minterm
+            if uses == 1 and last not in essential_prime_implicants:
+                for v in last.get_values():
+                    values_used[self._values.index(v)] = True
+                essential_prime_implicants.append(last)
+        
+        #모든 값이 사용되었는지 확인
+        if values_used.count(False) == 0:
+            return essential_prime_implicants
+        
+        #the fewest, largest circle 확인
+        prime_implicants = [prime_implicant for prime_implicant in prime_implicants if prime_implicant not in essential_prime_implicants]
+
+        #하나의 implicant만 남았는지 확인
+        if len(prime_implicants) == 1:
+            return essential_prime_implicants + prime_implicants
+
+        # Create a power set from the remaining prime implicants and check which
+        #   combination of prime implicants gets the simplest form
+        return essential_prime_implicants + self.__power_set([
+            self._values[index]
+            for index in range(len(self._values))
+            if not values_used[index]
+        ], prime_implicants)
